6a32f034c3f7b4856b055d5d4f5fc97736cd18a8
Cameron <cameron@iMac-11.local>
2022-04-25 16:55:33 -0500

    update

--- src/pages/books/Books.js
+++ src/pages/books/Books.js
@@ -6,7 +6,7 @@ const Books = ({fontColor, color, setFontColor, setColor}) => {
   return (
     <div className="books-container">
         <h1 style={{width: "100%", height: "fit-content",textAlign: "center", padding: "15px 5px", color: fontColor, backgroundColor: color, marginTop: "0"}}>Books</h1>
-        <Link to="/samplereader" style={{color: fontColor, backgroundColor: color, padding: "15px", borderRadius: "10px", margin: "auto"}}>go to sample reader</Link>
+        <Link className="sample-reader-btn" to="/samplereader" style={{color: fontColor, backgroundColor: color, padding: "15px", borderRadius: "10px", margin: "auto"}}>go to sample reader</Link>
     </div>
   )
 }
